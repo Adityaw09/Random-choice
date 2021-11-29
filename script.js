4b48efc38969dada01bfa82cf91fb44bad72f19c
unknown <adityaw097@gmail.com>
2021-11-29 17:25:52 +0530

    randomchoice

---
+++ script.js
@@ -0,0 +1,70 @@
+//assinging variables
+const textarea= document.getElementById("textarea")
+const tagsEl=document.getElementById("tags")
+
+textarea.focus()
+
+textarea.addEventListener("keyup" , (e)=>{
+    createTags(e.target.value)
+
+    if(e.key==="Enter"){
+        setTimeout(()=>{
+            e.target.value=""
+        }, 10)
+        randomSelect()
+    }
+})
+
+function createTags(input){
+  const tags=  input.split(",").filter( tag=> tag.trim() !=="" ).map(tag => tag.trim())
+    //console.log(input)
+    tagsEl.innerHTML=""//clear values
+
+    tags.forEach(tag=>{
+        const tagEl=document.createElement("span")
+        tagEl.classList.add("tag")
+        tagEl.innerText= tag
+        tagsEl.appendChild(tagEl)
+    })
+}
+function randomSelect(){
+    const times=30; //assigning multiplier
+
+   const interval= setInterval(() => {
+        const randomTag=pickRandomTag();
+
+        if(randomTag !== undefined){
+            highlightTag(randomTag)
+            setTimeout(() => {
+                unHighLightTag(randomTag)
+            },100)
+
+        }
+        
+    }, 100);
+
+    setTimeout(() => {
+        clearInterval(interval)
+
+        setTimeout(()=>{
+            const randomTag = pickRandomTag()
+            highlightTag(randomTag)
+
+        },100)
+    }, times * 100)
+}
+function pickRandomTag(){
+   const tags= document.querySelectorAll(".tag")
+    return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag){
+    tag.classList.add("highlight")
+}
+
+function unHighLightTag(tag){
+    tag.classList.remove("highlight")
+}
+
+
+
